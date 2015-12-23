17cc82a1feb0d69d5b7133665b826a50cfb37692
thekensta <chris@thekensta.com>
2015-12-23 17:31:06 +0000

    Fixes to test script packaging

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# stathack
+# stathacks
 Practical statistics using resampling
+
+Hopefully a more strucutred home for all the stuff I use regularly supporting (currently) split testing 
+
+Currently just checking packaging is all OK
+
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
+# http://python-packaging.readthedocs.org/en/latest/minimal.html
 setup(name="stathacks",
       packages=["stathacks"],
       version="0.1",
       license="MIT", 
       url="https://github.com/thekensta/stathacks",
-      install_requires=["numpy>=1.6", "scipy>=0.16"]
+      install_requires=["numpy>=1.6", "scipy>=0.16"], 
+      scripts=["bin/ab-test"]
      )
 
